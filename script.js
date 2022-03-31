5aa9ed13c577bf41f16dd44bef1c6fcfbe2d1c32
Sergio Valadez <sergioval@gmail.com>
2022-03-31 11:10:12 -0600

    Add full smallcups function

--- script.js
+++ script.js
@@ -8,5 +8,16 @@ smallCups.forEach((cup, idx) => {
 })
 
 function highlightCups(idx) {
-    console.log(idx)
+    if (smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+
+        }
+    })
 }
