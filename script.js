fb20f5aac9f6ce8e7fd54329645de8bb5a22e155
Sergio Valadez <sergioval@gmail.com>
2022-03-31 10:49:04 -0600

    Add js file

---
+++ script.js
@@ -0,0 +1,8 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+smallCups.forEach((cup, idx) => {
+    console.log(idx)
+})
